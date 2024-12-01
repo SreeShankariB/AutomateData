96fa8fe6136e1f4e7d16171a9c6a0b4de5ca373b
SreeShankariB <b.sreeshankari@gmail.com>
2024-12-01 10:37:47 +0100

    Update scrap_intraday_data.py

--- scrap_intraday_data.py
+++ scrap_intraday_data.py
@@ -85,7 +85,7 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 
-df.to_csv('intraday_auction_data/intraday_auction_data_' + str(delivery_date) + '.csv', sep = ';')
+df.to_csv(os.path.join(output_dir, 'intraday_auction_data_' + str(delivery_date) + '.csv'), sep=';')
 
 
 # Intraday continuous data
